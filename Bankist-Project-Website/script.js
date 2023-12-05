2dfb1ad3b5688d014498372d56367f3a87f3781b
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2023-12-05 23:49:44 +0200

    build Lazy Loading imges functionality

--- Bankist-Project-Website/script.js
+++ Bankist-Project-Website/script.js
@@ -149,3 +149,26 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+/////////////////////////////////////////////////////
+// Lazy Loading //
+const allImgs = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px", // load before we reach it //
+});
+
+allImgs.forEach((img) => imgObserver.observe(img));
