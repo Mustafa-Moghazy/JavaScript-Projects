e11880d2fe9933364c750eced17631ad7cba21b7
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2023-12-05 23:10:30 +0200

    build revel sections

--- Bankist-Project-Website/script.js
+++ Bankist-Project-Website/script.js
@@ -131,3 +131,21 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+////////////////////////////////////////////////
+// revel sections ==> it's like fadein animation //
+const allSections = document.querySelectorAll(".section");
+const revelSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revelSection, {
+  root: null,
+  threshold: 0.15, // 15% //
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
