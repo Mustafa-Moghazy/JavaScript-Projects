6632827afe769419c21ba7344859f52e0a2ff9b0
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2023-12-13 20:21:44 +0200

    add leaflet library to render the map

--- Mapty-Project/script.js
+++ Mapty-Project/script.js
@@ -10,3 +10,38 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+// (in case of succes, in case of fail)
+
+navigator.geolocation.getCurrentPosition(
+  function (postion) {
+    const { latitude } = postion.coords;
+    const { longitude } = postion.coords;
+
+    const map = L.map("map").setView([latitude, longitude], 11);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on("click", function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+          })
+        )
+        .setPopupContent("workout")
+        .openPopup();
+    });
+  },
+  function () {
+    alert("couldn't load your loaction");
+  }
+);
